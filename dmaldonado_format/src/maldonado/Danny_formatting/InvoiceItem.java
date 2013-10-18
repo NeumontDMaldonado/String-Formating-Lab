1aed654d885a81b15dc647661da4ba74b5b03d51
NeumontDMaldonado <fallingdow@yahoo.com>
2013-10-18 08:49:40 -0600

    New fixed file

--- dmaldonado_format/src/maldonado/Danny_formatting/InvoiceItem.java
+++ dmaldonado_format/src/maldonado/Danny_formatting/InvoiceItem.java
@@ -6,19 +6,19 @@ public class InvoiceItem
 	int quantity;
 	double price;
 	int itemCounter=0, quantityCounter=0, priceCounter =0;
+	int longest, length1, length2, length3;
 
 	String[] invoiceItems = new String[3];
-//	int[] numOfItems = new int[3];
-	double[] pricing = new double[3];
 
 	public InvoiceItem()
 	{
-		invoiceItem = "Hard Drive";
+		invoiceItem = "Hard Drive Cable";
 		invoiceItemArray(invoiceItem);
 		invoiceItem = "HDMI Cable";
 		invoiceItemArray(invoiceItem);
 		invoiceItem = "SATA Cable";
 		invoiceItemArray(invoiceItem);
+		findLargestWord();
 	}
 
 	public void invoiceItemArray(String invoiceItem)
@@ -27,18 +27,39 @@ public class InvoiceItem
 		itemCounter++;
 	}
 	
+	public void findLargestWord()
+	{
+		length1 = invoiceItems[0].length();
+		length2 = invoiceItems[1].length();
+		length3 = invoiceItems[2].length();
+		longest = length1;
+		if(length2 > length1)
+		{
+			longest = length2;
+			if(length3 > length2)
+			{
+				longest = length3;
+			}
+		}
+		if(length3 > length1)
+		{
+			longest = length3;
+		}
+		
+	}
+	
 	public void printArray()
 	{
 		System.out.println("Item     Quantity Price" );
 		System.out.println("----     -------- -----");
 		quantity = 1;
 		price = 19.99;
-		System.out.format("[%s %d %s]\n" ,invoiceItems[0],quantity, price);
+		System.out.format("[%"+longest+ "s %"+10+"d %s]\n" ,invoiceItems[0],quantity, price);
 		quantity = 2;
 		price = 22.43;
-		System.out.format("[%s %d %s]\n" ,invoiceItems[1],quantity, price);
+		System.out.format("[%"+longest+ "s %"+10+"d %s]\n" ,invoiceItems[1],quantity, price);
 		quantity = 9;
 		price = 11.99;
-		System.out.format("[%s %d %s]\n" ,invoiceItems[2],quantity, price);
+		System.out.format("[%"+longest+ "s %"+10+"d %s]\n" ,invoiceItems[2],quantity, price);
 	}
 }
